b1b6513d36a724fcc5180246026b8648a3ed2c57
Ian Charters <ian@boot.rip>
2018-01-11 20:07:27 -0800

    Initial commit

---
+++ config/webpack/webpack.config.js
@@ -0,0 +1,54 @@
+// =============================================================================
+// Import modules
+// =============================================================================
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const paths = {
+  BUILD: path.resolve(__dirname, '../../build'),
+  ENTRY: path.resolve(__dirname, '../../entry'),
+  SRC: path.resolve(__dirname, '../../src'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.ENTRY, 'index.js'),
+  output: {
+    path: paths.BUILD,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.ENTRY, 'index.html'),
+    }),
+    new ExtractTextPlugin('bundle.css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'images/',
+          },
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
---
+++ entry/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import './index.css';
+import keenImage from '../assets/images/keen.png';
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+        <img src={ keenImage } alt='Commander Keen' />
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
